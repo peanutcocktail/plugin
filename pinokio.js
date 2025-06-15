dea320f23b1a512a3dd43741891faffd54fa4f8b
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-15 10:01:07 -0400

    init

---
+++ pinokio.js
@@ -0,0 +1,52 @@
+module.exports = {
+  menu: async (kernel, info) => {
+    console.log("cwd", info.cwd())
+    return [
+      {
+        text: "Dev",
+        icon: "fa-solid fa-code",
+        menu: [
+          {
+            icon: "fa-solid fa-terminal",
+            text: "Terminal",
+            shell: {
+              input: true
+            },
+          },
+          {
+            image: "/asset/plugin/dev/claude.png",
+            text: "Claude Code",
+            href: "/run/plugin/dev/claude.json",
+            params: {
+              cwd: info.cwd()
+            },
+          },
+          {
+            image: "/asset/plugin/dev/openai.webp",
+            text: "OpenAI Codex",
+            href: "/run/plugin/dev/codex.json",
+            params: {
+              cwd: info.cwd()
+            },
+          },
+          {
+            image: "/asset/plugin/dev/cursor.jpeg",
+            text: "Cursor",
+            run: {
+              message: "cursor .",
+              path: info.cwd()
+            }
+          },
+          {
+            image: "/asset/plugin/dev/windsurf.png",
+            text: "Windsurf",
+            run: {
+              message: "windsurf .",
+              path: info.cwd()
+            }
+          }
+        ],
+      },
+    ]
+  }
+}
